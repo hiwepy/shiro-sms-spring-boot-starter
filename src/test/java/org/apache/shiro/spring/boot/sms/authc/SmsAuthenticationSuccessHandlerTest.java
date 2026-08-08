3f5c92ef962359bda4d0a7ab97382e49854617f0
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:47 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/test/java/org/apache/shiro/spring/boot/sms/authc/SmsAuthenticationSuccessHandlerTest.java
+++ src/test/java/org/apache/shiro/spring/boot/sms/authc/SmsAuthenticationSuccessHandlerTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {{ @link SmsAuthenticationSuccessHandler }}.
  *
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  * @since 1.0.0
  */
 @DisplayName("SmsAuthenticationSuccessHandler Tests")
